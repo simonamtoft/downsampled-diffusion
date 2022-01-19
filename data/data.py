b0e069aaf86c8356c2c1089d7019160200d805ae
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2022-01-19 15:38:01 +0100

    add mnist

--- data/data.py
+++ data/data.py
@@ -1,22 +1,44 @@
+import torch
+import numpy as np
 from torchvision.transforms import Compose, Resize, \
     CenterCrop, ToTensor, Normalize
-from torchvision.datasets import CelebA, CIFAR10, CIFAR100
+from torchvision.datasets import CelebA, CIFAR10, \
+    CIFAR100, MNIST
 from torch.utils.data import DataLoader
 
 DATA_ROOT = './data/'
 
 
 def download_datasets(data_root=DATA_ROOT) -> None:
-    """Downloads the following datasets: Cifar10, Cifar100, ..."""
+    """Downloads the following datasets: CIFAR10, CIFAR100 & MNIST"""
     # _ = CelebA(DATA_ROOT, download=True)
     print('Downloading CIFAR10')
     _ = CIFAR10(data_root, download=True)
     print('Downloading CIFAR100')
     _ = CIFAR100(data_root, download=True)
+    print('Downloadning MNIST')
+    _ = MNIST(data_root, download=True)
+    print('Finished downloading CIFAR10, CIFAR100 & MNIST...')
 
 
-def get_dataloader(config, train=True, data_root=DATA_ROOT) -> DataLoader:
-    """Returns dataloader for dataset specified in config"""
+def get_dataloader(config, train=True, data_root=DATA_ROOT, val_split=0.15) -> DataLoader:
+    """ Returns dataloaders for train and validation splits 
+        of the dataset specified in config.
+        
+    Args
+        config (dict):      A dict determining the 'dataset' to use and the resulting 
+                            'image_size' of the images in the resulting DataLoader.
+        train (bool):       A boolean value that determines whether to take the train or test set.
+        data_root (str):    A string that describes the path to the downloaded data.
+        val_split (float):  A float that determines how much of the data that should be in 
+                            the validation set [0; 1]. Ignored if train = False.
+    
+    Returns
+        train_set (DataLoader): If train=True, this dataloader contains the shuffled training
+                                samples.
+        val_set (DataLoader):   If train=True, this DataLoader contains the validation samples.
+        test_set (DataLoader):  If train=False, this DataLoader contains the test samples.
+    """
 
     # Define transforms to perform on each individual image
     data_transform = [
@@ -33,16 +55,42 @@ def get_dataloader(config, train=True, data_root=DATA_ROOT) -> DataLoader:
         data = CIFAR10(data_root, **data_args)
     elif config['dataset'] == 'cifar100':
         data = CIFAR100(data_root, **data_args)
+    elif config['dataset'] == 'mnist':
+        data = MNIST(data_root, **data_args)
     else:
         raise Exception(f'Dataset {config["dataset"]} not implemented...')
+    
+    # return train and validation DataLoaders
+    if train:
+        # define number of samples in train and validation sets
+        split = (len(data) * np.array([1-val_split, val_split])).astype(int)
 
-    # Return DataLoader for data
-    return DataLoader(
-        data, 
-        batch_size=config['batch_size'], 
-        shuffle=True, 
-        drop_last=True
-    )
+        # split data into train and validation
+        train_data, val_data = torch.utils.data.random_split(data, list(split))
+
+        # Create and return dataloaders for each set
+        train_set = DataLoader(
+            train_data,
+            batch_size=config['batch_size'], 
+            shuffle=True, 
+            drop_last=True
+        )
+        val_set = DataLoader(
+            val_data,
+            batch_size=config['batch_size'], 
+            shuffle=False, 
+            drop_last=True
+        )
+        return train_set, val_set
+    # return test DataLoader
+    else:
+        test_set = DataLoader(
+            data,
+            batch_size=config['batch_size'], 
+            shuffle=False, 
+            drop_last=True
+        )
+        return test_set
 
 
 def get_label_map(dataset):
