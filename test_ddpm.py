f48b45de88317b89eb55702fad0c61250093583f
simonamtoft <s173936@student.dtu.dk>
2022-01-21 18:01:11 +0100

    fix channel matching

--- test_ddpm.py
+++ test_ddpm.py
@@ -14,6 +14,7 @@ DATA_ROOT = '../data/'
 
 # define config
 config = {
+    'model': 'ddpm_small',
     'dataset': 'mnist',
     'image_size': 28,
     'batch_size': 32,
@@ -29,24 +30,32 @@ config['device'] = 'cuda' if cuda else 'cpu'
 
 # select number of input channels dependent on data
 # (number of color channels of the images in the dataset)
+
 if config['dataset'] == 'mnist':
     color_channels = 1
 else:
     color_channels = 3
 
+if config['model'] == 'ddpm_small':
+    latent_channels = 2*config['image_size']
+else:
+    latent_channels = color_channels
+
 # Define Latent Architecture
-unet_dims = (1, 2, 4) #, 8
+unet_dims = (1, 2) #, 4, 8
 print(f"U-net with {unet_dims}")
 model = Unet(
     dim=64,
-    channels=color_channels,
+    channels=latent_channels,
     dim_mults=unet_dims,
 ).to(config['device'])
 
 # Define Diffusion Model
 model_args = {'image_size': config['image_size'], 'timesteps': config['timesteps'], 'loss_type': config['loss_type'], 'channels': color_channels}
-# diffusion = GaussianDiffusion(model, **model_args).to(config['device'])
-diffusion = GaussianDiffusionSmall(model, **model_args).to(config['device'])
+if config['model'] == 'ddpm_small':
+    diffusion = GaussianDiffusionSmall(model, **model_args).to(config['device'])
+else:
+    diffusion = GaussianDiffusion(model, **model_args).to(config['device'])
 
 # load in data
 train_loader, val_loader = get_dataloader(config, data_root=DATA_ROOT)
