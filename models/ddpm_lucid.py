bd8562efb1e7bde36baaa06058699b7c67a14814
Simon Amtoft Pedersen <simon.ipsum@gmail.com>
2022-01-21 16:26:46 +0100

    refactor

--- models/ddpm_lucid.py
+++ models/ddpm_lucid.py
@@ -7,32 +7,8 @@ import torch.nn.functional as F
 from pathlib import Path
 from functools import partial
 
-from .helpers import default, exists
-
-
-def extract(a, t, x_shape):
-    b, *_ = t.shape
-    out = a.gather(-1, t)
-    return out.reshape(b, *((1,) * (len(x_shape) - 1)))
-
-
-def noise_like(shape, device, repeat=False):
-    repeat_noise = lambda: torch.randn((1, *shape[1:]), device=device).repeat(shape[0], *((1,) * (len(shape) - 1)))
-    noise = lambda: torch.randn(shape, device=device)
-    return repeat_noise() if repeat else noise()
-
-
-def cosine_beta_schedule(timesteps, s=0.008):
-    """
-    cosine schedule
-    as proposed in https://openreview.net/forum?id=-NEXDKk8gZ
-    """
-    steps = timesteps + 1
-    x = np.linspace(0, steps, steps)
-    alphas_cumprod = np.cos(((x / steps) + s) / (1 + s) * np.pi * 0.5) ** 2
-    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
-    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
-    return np.clip(betas, a_min = 0, a_max = 0.999)
+from .helpers import default, exists, \
+    extract, noise_like, cosine_beta_schedule
 
 
 class GaussianDiffusion(nn.Module):
