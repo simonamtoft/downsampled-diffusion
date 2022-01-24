fada24cd9e831f1d446b17b3d5a7bea1411a32fd
simonamtoft <s173936@student.dtu.dk>
2022-01-24 13:01:52 +0100

    rename

--- train_ddpm.py
+++ train_ddpm.py
@@ -1,10 +1,8 @@
-from models import Unet, GaussianDiffusion, \
-    GaussianDiffusionSmall
+from models import Unet, GaussianDiffusion
 from trainers import DDPM_Trainer
 from utils import get_dataloader
 import torch
 import os
-
 
 # mute weight and biases prints
 os.environ["WANDB_SILENT"] = "true"
@@ -14,12 +12,12 @@ DATA_ROOT = '../data/'
 
 # define config
 config = {
-    'model': 'ddpm_small',
+    'model': 'ddpm',
     'dataset': 'mnist',
     'image_size': 28,
     'batch_size': 32,
-    'loss_type': 'l1', # l1 or l2
-    'timesteps': 1000,
+    'loss_type': 'l1',  # l1 or l2
+    'timesteps': 1000,    # how deep the ddpm is
     'train_steps': 700000,
     'lr': 2e-5,
 }
@@ -30,32 +28,23 @@ config['device'] = 'cuda' if cuda else 'cpu'
 
 # select number of input channels dependent on data
 # (number of color channels of the images in the dataset)
-
 if config['dataset'] == 'mnist':
     color_channels = 1
 else:
     color_channels = 3
 
-if config['model'] == 'ddpm_small':
-    latent_channels = 2*config['image_size']
-else:
-    latent_channels = color_channels
-
-# Define Latent Architecture
-unet_dims = (1, 2) #, 4, 8
+# Instantiate Latent Architecture
+unet_dims = (1, 2, 4) #, 8
 print(f"U-net with {unet_dims}")
 model = Unet(
     dim=64,
-    channels=latent_channels,
+    channels=color_channels,
     dim_mults=unet_dims,
 ).to(config['device'])
 
-# Define Diffusion Model
+# Instantiate Diffusion Model
 model_args = {'image_size': config['image_size'], 'timesteps': config['timesteps'], 'loss_type': config['loss_type'], 'channels': color_channels}
-if config['model'] == 'ddpm_small':
-    diffusion = GaussianDiffusionSmall(model, **model_args).to(config['device'])
-else:
-    diffusion = GaussianDiffusion(model, **model_args).to(config['device'])
+diffusion = GaussianDiffusion(model, **model_args).to(config['device'])
 
 # load in data
 train_loader, val_loader = get_dataloader(config, data_root=DATA_ROOT)
