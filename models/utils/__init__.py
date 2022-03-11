84550683eb6d1ba6088c0fd94834ee3d21a4baac
simonamtoft <s173936@student.dtu.dk>
2022-03-11 21:14:28 +0100

    new flat

--- models/utils/__init__.py
+++ models/utils/__init__.py
@@ -1,5 +1,5 @@
 from .helpers import exists, get_identity_like, \
     get_ones_like, default, extract, noise_like, \
-    flat_bits, flat_nats
+    flat_bits, reduce_mean, reduce_sum
 from .losses import l1_loss, l2_loss, normal_kl, \
     discretized_gaussian_log_likelihood
