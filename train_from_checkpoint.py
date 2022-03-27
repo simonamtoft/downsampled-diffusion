10a8bb6f3be41085b54bdace56e67043e5cde953
simonamtoft <s173936@student.dtu.dk>
2022-03-27 13:37:26 +0200

    remove fixed loss_flat for config

--- train_from_checkpoint.py
+++ train_from_checkpoint.py
@@ -6,19 +6,15 @@ from trainers import setup_trainer
 DATA_ROOT = '../data/'
 RES_FOLDER = './results'
 WANDB_PROJECT = 'ddpm-test'
-CHECKPOINT_NAME = 'checkpoint_ddpm_vlb_paper_3.pt'
+CHECKPOINT_NAME = 'checkpoint_ddpm_vlb_paper_4.pt'
 # CHECKPOINT_NAME = 'checkpoint_ddpm_simple_paper_4.pt'
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
+    # load checkpoint
     checkpoint = torch.load(os.path.join(RES_FOLDER, CHECKPOINT_NAME))
     config = checkpoint['config']
-    config['loss_flat'] = 'mean'
-    
-    # instantiate trainer
     trainer, config = setup_trainer(config, True, DATA_ROOT, WANDB_PROJECT, RES_FOLDER, seed=0)
-
-    # load checkpoint into trainer
     trainer.load_checkpoint(checkpoint)
     
     # print out train configuration
@@ -27,7 +23,7 @@ if __name__ == '__main__':
     print('Using configuration dict:')
     print(json.dumps(config, sort_keys=False, indent=4) + '\n')
     
-    # start training
+    # start training from checkpoint
     trainer.train()
     
     print("train_from_checkpoint.py script finished!")
