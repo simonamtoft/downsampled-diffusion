5a73e5c3b952e0294a233abf5eac483caf5fb975
simonamtoft <s173936@student.dtu.dk>
2022-04-04 18:36:07 +0200

    add stuff for u_mode

--- train_from_checkpoint.py
+++ train_from_checkpoint.py
@@ -6,14 +6,19 @@ from trainers import setup_trainer
 DATA_ROOT = '../data/'
 RES_FOLDER = './results'
 WANDB_PROJECT = 'ddpm-test'
-CHECKPOINT_NAME = 'checkpoint_ddpm_vlb_paper_4.pt'
-# CHECKPOINT_NAME = 'checkpoint_ddpm_simple_paper_4.pt'
-
+CHECKPOINT_NAME = 'chq_x3_t100_d_3.pt'
+# chq_x3_t10_2.pt
+# chq_x3_t100_4.pt
+# chq_x3_t100_d_2.pt
+# chq_x3_AE_3.pt
 
 if __name__ == '__main__':
     # load checkpoint
-    checkpoint = torch.load(os.path.join(RES_FOLDER, CHECKPOINT_NAME))
+    checkpoint = torch.load(os.path.join(RES_FOLDER, 'checkpoints', CHECKPOINT_NAME))
     config = checkpoint['config']
+    if config['model'] == 'dddpm':
+        if 'u_mode' not in config:
+            config['u_mode'] = config['d_mode']
     trainer, config = setup_trainer(config, True, DATA_ROOT, WANDB_PROJECT, RES_FOLDER, seed=0)
     trainer.load_checkpoint(checkpoint)
     
