b15dcc576fd21474f6692a7600bbbec67b8483ec
simonamtoft <s173936@student.dtu.dk>
2022-04-19 12:53:50 +0200

    add timing

--- generate_model_samples.py
+++ generate_model_samples.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 import torch
 import numpy as np
 from tqdm import tqdm
@@ -9,12 +10,8 @@ from utils import get_color_channels, fix_samples, \
     CHECKPOINT_DIR, SAMPLE_DIR
 
 device = 'cuda'
-saved_model = 'chq_x3_t100_4'
-fid_samples = 1
-# chq_x3_AE_4
-# chq_x3_AE_rnd_???
-# chq_x3_t100_4
-# chq_x3_t100_d_3
+saved_model = 'chq_x3_AE_d_3'
+fid_samples = 10000
 
 # load saved state dict of model and its config file
 save_data = torch.load(os.path.join(CHECKPOINT_DIR, f'{saved_model}.pt'))
@@ -40,6 +37,7 @@ print(f'\nGenerating samples from checkpoint {saved_model} with configuration di
 print(json.dumps(config, sort_keys=False, indent=4) + '\n')
 sample_list = []
 # latent_list = []
+time_start = time.time()
 for i in tqdm(range(int(np.ceil(fid_samples/config['batch_size']))), desc='sampling from model'):
     samples = model.sample(config['batch_size'])
     if config['model'] == 'dddpm':
@@ -48,9 +46,15 @@ for i in tqdm(range(int(np.ceil(fid_samples/config['batch_size']))), desc='sampl
         # latent_list.append(fix_samples(latent_samples))
     # else:
     sample_list.append(fix_samples(samples))
+sampling_time = time.time() - time_start
+
+# Input space samples
 save_path = os.path.join(SAMPLE_DIR, saved_model)
 np.save(save_path, sample_list, allow_pickle=False)
+print(f'Samples saved to {save_path}')
+print(f'Total time: {sampling_time}')
+print(f'Sample time: {sampling_time/fid_samples}')
+# Latent samples
 # if config['model'] == 'dddpm':
 #     np.save(f'./results/samples/{saved_model}_latent', latent_list, allow_pickle=False)
 #     print(f'Latent samples saved to ./results/samples/{saved_model}_latent')
-print(f'Samples saved to {save_path}')
