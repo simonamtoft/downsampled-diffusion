c27854416e3f0ca29a4dfe2dd999817300d3989f
simonamtoft <s173936@student.dtu.dk>
2022-04-26 12:14:26 +0200

    also save latent sample

--- generate_model_samples.py
+++ generate_model_samples.py
@@ -10,8 +10,10 @@ from utils import get_color_channels, fix_samples, \
     CHECKPOINT_DIR, SAMPLE_DIR
 
 device = 'cuda'
-saved_model = 'chq_x3_AE_d_3'
+saved_model = 'chq_x3_AE_4'
 fid_samples = 10000
+batch_size = 192
+# sample_every = 10
 
 # load saved state dict of model and its config file
 save_data = torch.load(os.path.join(CHECKPOINT_DIR, f'{saved_model}.pt'))
@@ -20,6 +22,12 @@ if 'ema_model' in save_data:
     model_state_dict = save_data['ema_model']
 else:
     model_state_dict = save_data['model']
+config['batch_size'] = batch_size
+
+if 'force_latent' not in config:
+    config['force_latent'] = False
+if 'rnd_flip' not in config:
+    config['rnd_flip'] = False
 
 # Setup DDPM model
 latent_model = Unet(config)
@@ -36,25 +44,34 @@ model.eval()
 print(f'\nGenerating samples from checkpoint {saved_model} with configuration dict:')
 print(json.dumps(config, sort_keys=False, indent=4) + '\n')
 sample_list = []
-# latent_list = []
+latent_list = []
 time_start = time.time()
-for i in tqdm(range(int(np.ceil(fid_samples/config['batch_size']))), desc='sampling from model'):
-    samples = model.sample(config['batch_size'])
+n_batches = int(np.ceil(fid_samples/config['batch_size']))
+for i in tqdm(range(n_batches), desc='sampling from model'):
+    samples = model.sample(config['batch_size']) #, sample_every
     if config['model'] == 'dddpm':
         samples, latent_samples = samples[0], samples[1]
-        # sample_list.append(fix_samples(samples))
-        # latent_list.append(fix_samples(latent_samples))
-    # else:
-    sample_list.append(fix_samples(samples))
+        sample_list.append(fix_samples(samples))
+        latent_list.append(fix_samples(latent_samples))
+    else:
+        sample_list.append(fix_samples(samples))
 sampling_time = time.time() - time_start
+
+# print stats
+print(f'Using batch size {config["batch_size"]}')
+print(f'Total time: {sampling_time}')
+print(f'Sample time: {sampling_time/fid_samples}')
+print(f'Batch time: {sampling_time/n_batches}')
+
+saved_model += '_s10'
 
 # Input space samples
 save_path = os.path.join(SAMPLE_DIR, saved_model)
 np.save(save_path, sample_list, allow_pickle=False)
 print(f'Samples saved to {save_path}')
-print(f'Total time: {sampling_time}')
-print(f'Sample time: {sampling_time/fid_samples}')
+
 # Latent samples
-# if config['model'] == 'dddpm':
-#     np.save(f'./results/samples/{saved_model}_latent', latent_list, allow_pickle=False)
-#     print(f'Latent samples saved to ./results/samples/{saved_model}_latent')
+if config['model'] == 'dddpm':
+    save_path = os.path.join(SAMPLE_DIR, f'{saved_model}_latent')
+    np.save(save_path, latent_list, allow_pickle=False)
+    print(f'Latent samples saved to {save_path}')
