3beb4efdfb17d2069956552ba531aa0fec88f9db
simonamtoft <s173936@student.dtu.dk>
2022-04-26 12:14:42 +0200

    some stuff might be missing from older configs

--- train_from_checkpoint.py
+++ train_from_checkpoint.py
@@ -6,8 +6,10 @@ from trainers import setup_trainer
 from utils import CHECKPOINT_DIR, DATA_DIR
 
 WANDB_PROJECT = 'ddpm-test'
-CHECKPOINT_NAME = 'chq_x3_AE_rnd_2.pt'
-
+CHECKPOINT_NAME = 'chq64_1.pt'
+# chq_x3_AE_rnd_2
+# chq_x3_AE_latent_3
+# chq64_1
 
 if __name__ == '__main__':
     # load checkpoint
@@ -16,6 +18,12 @@ if __name__ == '__main__':
     if config['model'] == 'dddpm':
         if 'u_mode' not in config:
             config['u_mode'] = config['d_mode']
+        if 'rnd_flip' not in config:
+            print('Setting "rnd_flip" in config to False')
+            config['rnd_flip'] = False
+        if 'force_latent' not in config:
+            print('Setting "force_latent" in config to False')
+            config['force_latent'] = False
     trainer, config = setup_trainer(config, True, DATA_DIR, WANDB_PROJECT, seed=0)
     trainer.load_checkpoint(checkpoint)
     
